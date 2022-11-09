1949d80d6e2f15957d1cc1197237686983f5184f
Abdullah Ayoola <100965547+abdullah43577@users.noreply.github.com>
2022-11-09 14:51:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![Screenshots](./design/Screenshot_4.png)
+![Screenshots](./design/Screenshot_5.png)
+![Screenshots](./design/Screenshot_6.png)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Desktop-first workflow
+
+### What I learned
+
+I learnt how to use the disabled properties of button. This is my first project I implemented using the disabled properties of the button and activated back after certain conditions has been met to delete some properties from the DOM.
+
+To see the code snippets, see below:
+
+```html
+<button disabled>Reset</button>
+```
+
+```css
+button:disabled {
+  color: #666;
+  background-color: gray;
+}
+```
+
+```js
+reset.disabled = true;
+```
+
+## Author
+
+- Website - [Abdullah Ayoola](https://github.com/abdullah43577)
+- Frontend Mentor - [@abdullah43577](https://www.frontendmentor.io/profile/abdullah43577)
+- Twitter - [@officialayo540](https://twitter.com/officialayo540)
---
+++ script.js
@@ -0,0 +1,91 @@
+const inputEl = document.querySelectorAll(".input");
+const tips = document.querySelectorAll(".box");
+const data = document.querySelectorAll(".__tipAmountSect > h2");
+const errorMessage = document.querySelector(".error");
+const reset = document.querySelector("button");
+const customBox = document.querySelector(".boxCustom");
+const customInput = document.querySelector(".customInput");
+
+let bill;
+let people_No;
+let tipsConverter;
+let newtipsConverter;
+let result;
+let result2;
+let finalResult;
+
+for (let i = 0; i < tips.length; i++) {
+  tips[i].addEventListener("click", () => {
+    // console.log(tips[i].innerText);
+    tips[i].classList.toggle("box__active");
+
+    // converting the values of the input boxes to Number
+    bill = Number(inputEl[0].value);
+    people_No = Number(inputEl[1].value);
+    console.log(people_No);
+    tipsConverter = tips[i].innerText;
+    newtipsConverter = operand(tipsConverter);
+
+    // console.log(bill);
+    // console.log(people_No);
+    // console.log(newtipsConverter);
+
+    if (people_No < 1) {
+      errorMessage.textContent = `Can't be zero`;
+      inputEl[1].classList.add("borderColor");
+
+      setTimeout(() => {
+        errorMessage.textContent = ``;
+        inputEl[1].classList.remove("borderColor");
+      }, 3000);
+    } else {
+      // doing the calculations and converting to D.P with the .toFixed() method
+      result = ((bill * (newtipsConverter / 100)) / people_No).toFixed(2);
+      console.log(result);
+
+      result2 = (bill * newtipsConverter) / 100;
+      finalResult = ((bill + result2) / people_No).toFixed(2);
+
+      data[0].innerHTML = `$${result}`;
+      data[1].innerHTML = `$${finalResult}`;
+    }
+
+    reset.disabled = false;
+
+    function operand(value) {
+      // splitting the default data that comes into this function into an array and assigning to Data
+      const Data = value.split("");
+
+      // popping off the last element of the array since it'd always be the percentage
+      const newData = Data.pop();
+
+      // concatenating the array back into string
+      const updatedData = Data.join("");
+
+      // returning the converted version of the updatedData (Number)
+      return Number(updatedData);
+    }
+  });
+}
+
+reset.addEventListener("click", () => {
+  console.log("I'm being clicked");
+  inputEl[0].value = "";
+  inputEl[1].value = "";
+
+  data[0].innerHTML = `$0.00`;
+  data[1].innerHTML = `$0.00`;
+
+  for (let i = 0; i < tips.length; i++) {
+    tips[i].classList.remove("box__active");
+  }
+
+  reset.disabled = true;
+});
+
+customBox.addEventListener("click", () => {
+  console.log("I'm being clicked");
+  // customBox.innerHTML = `<input type='text' class="customInput">`;
+  //   customBox.classList.add("customInput");
+  customInput.classList.remove("hidden");
+});
